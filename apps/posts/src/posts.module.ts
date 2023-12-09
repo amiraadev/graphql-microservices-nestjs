eebb0a3e8548db4b4d16e477304b825291d73d6d
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-09 14:17:29 +0100

    resource for posts

--- apps/posts/src/posts.module.ts
+++ apps/posts/src/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
