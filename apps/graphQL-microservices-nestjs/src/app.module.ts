25492f3c5448711e1bcbc28ea193f092c5880ceb
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-09 17:45:40 +0100

    20

--- apps/graphQL-microservices-nestjs/src/app.module.ts
+++ apps/graphQL-microservices-nestjs/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      // server: {
+      //   cors: true,
+      // },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:3000/graphql',
+            },
+            {
+              name: 'posts',
+              url: 'http://localhost:3001/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
