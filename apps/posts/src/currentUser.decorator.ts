2c8ddeb4702a28dc0b7df3aa8b3e81a673dc9e30
amiraallagui <allagui_fawzi@outlook.fr>
2023-12-09 19:09:14 +0100

    27

--- apps/posts/src/currentUser.decorator.ts
+++ apps/posts/src/currentUser.decorator.ts
@@ -4,7 +4,10 @@ import { GraphQLExecutionContext } from '@nestjs/graphql';
 export const CurrentUser = createParamDecorator(
   (_data: any, ctx: GraphQLExecutionContext) => {
     try {
-      const headers = ctx.getArgs()[2];
+      const headers = ctx.getArgs()[2].req.headers;
+      if (headers.user) {
+        return JSON.parse(headers.user);
+      }
     } catch (err) {
       return null;
     }
